afb62cb31219536c1efa54497f75e9816dfab245
tanianasrin1 <tasrineva169@gmail.com>
2022-02-27 17:34:24 +0600

    url name

--- app.js
+++ app.js
@@ -3,18 +3,59 @@ const searchPlayers = () =>{
     const inputvalue = (input.value);
     // console.log(inputvalue)
     const error = document.getElementById('error')
+
     // console.log(error)
     
      if (inputvalue == ''){
         error.innerText = 'Enter the number'
+        input.value = ''
     }
     
-    else if( inputvalue != 'number'){
-        error.innerText = 'please Enter the valid number'
+    //  if( inputvalue != 'number'){
+    //     error.innerText = 'please Enter the valid number'
+    //     input.value = ''
+    // }
+
+     if(inputvalue <= 0){
+        error.innerText = 'Enter the Positive number'
+        input.value = ''
     }
 
-    else if(inputvalue <= 0){
-        error.innerText = 'Enter the Positive number'
-    }
+     
+      else{
+        const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${inputvalue}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayPlayers(data.player))
+        input.value = ''
+    
+      }
+
+}
+
+const displayPlayers = (players) => {
+ for(const player of players){
+    //  console.log(player.strThumb)
+    const playerContainer = document.getElementById('player-container')
+    const div = document.createElement('div')
+    div.innerHTML = `
+    <div class="card border p-5">
+    <div class="pro-pic">
+ 
+    <img class="w-50" src="${player.strThumb}" alt="">
+    </div>
+    <h2>Name: ${player.strPlayer}</h2>
+    <h5>Country: ${player.strNationality}</h5>
+    <p></p>
+    <div class="all-button"> 
+        <button class="btn btn-danger">Delete</button>
+        <button class="btn btn-success">Details</button>
+ 
+    </div>
+ 
+ </div>
+    `
+    playerContainer.appendChild(div)
+ }
 
 }
---
+++ text.js
@@ -0,0 +1,20 @@
+const inputvalue = (input.value);
+// console.log(inputvalue)
+const error = document.getElementById('error')
+
+// console.log(error)
+
+//  if (inputvalue == ''){
+//     error.innerText = 'Enter the number'
+//     input.value = ''
+// }
+
+//  if( inputvalue != 'number'){
+//     error.innerText = 'please Enter the valid number'
+//     input.value = ''
+// }
+
+//  if(inputvalue <= 0){
+//     error.innerText = 'Enter the Positive number'
+//     input.value = ''
+// }
